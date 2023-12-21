2c8013180b186f8fc13b1e8726b13efc9c8ca1ab
PietroC21 <pietrocandiani@Pietros-Air.attlocal.net>
2023-12-20 19:00:23 -0500

    First Commit

---
+++ InstaFollower/InstaFollowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFollowerApp.swift
+//  InstaFollower
+//
+//  Created by Pietro Candiani on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFollowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
